5baea8f66fc4a314318c56fa5e2cbb12e66cf2f2
qpwo5470 <37436911+qpwo5470@users.noreply.github.com>
2020-07-07 14:04:28 +0900

    main no try

--- main.py
+++ main.py
@@ -18,45 +18,44 @@ def runBrowser(n, q):
     global url
     global minutes
     while True:
-        try:
-            with Controller.from_port(port=9053 + n * 2) as c:
-                c.authenticate("steamviewbot")
-                c.signal(Signal.NEWNYM)
-            socks = 'socks5://127.0.0.1:' + str(9052 + n * 2)
-            proxies = {
-                'http': socks,
-                'https': socks
-            }
+        with Controller.from_port(port=9053 + n * 2) as c:
+            c.authenticate("steamviewbot")
+            c.signal(Signal.NEWNYM)
+        socks = 'socks5://127.0.0.1:' + str(9052 + n * 2)
+        proxies = {
+            'http': socks,
+            'https': socks
+        }
 
-            my_ip = requests.get('https://api.ipify.org', proxies=proxies).text
-            if my_ip in used_ip:
-                continue
-            else:
-                used_ip[my_ip] = 0
-    
-                opts = Options()
-                opts.log.level = "trace"
-                opts.headless = True
-                profile = webdriver.FirefoxProfile()
-                profile.set_preference("network.proxy.type", 1)
-                profile.set_preference("network.proxy.socks", "127.0.0.1")
-                profile.set_preference("network.proxy.socks_port", 9052 + n * 2)
-                profile.set_preference('general.useragent.override', UserAgent().random)
-                profile.update_preferences()
-                driver = webdriver.Firefox(profile, options=opts)
-                driver.get(url)
-                # data = driver.find_element_by_xpath('/html/body/div/div[2]/div[4]/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td[5]/br[6]')
-                # data = [driver.page_source]
+        my_ip = requests.get('https://api.ipify.org', proxies=proxies).text
+        if my_ip in used_ip:
+            continue
+        else:
+            used_ip[my_ip] = 0
 
-                q.put(my_ip)
-                wait = random.randrange(minutes * 60, int(minutes*1.2) * 60)
-                # wait = random.randrange(60, 120)
-                time.sleep(wait)
-                driver.stop_client()
-                driver.close()
-                driver.quit()
-        except:
-            pass
+            opts = Options()
+            opts.log.level = "trace"
+            opts.headless = True
+            profile = webdriver.FirefoxProfile()
+            profile.set_preference("network.proxy.type", 1)
+            profile.set_preference("network.proxy.socks", "127.0.0.1")
+            profile.set_preference("network.proxy.socks_port", 9052 + n * 2)
+            profile.set_preference('general.useragent.override', UserAgent().random)
+            profile.update_preferences()
+            driver = webdriver.Firefox(profile, options=opts)
+            driver.get(url)
+            # data = driver.find_element_by_xpath('/html/body/div/div[2]/div[4]/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td[5]/br[6]')
+            # data = [driver.page_source]
+
+            q.put(my_ip)
+            wait = random.randrange(minutes * 60, int(minutes*1.2) * 60)
+            # wait = random.randrange(60, 120)
+            time.sleep(wait)
+            driver.stop_client()
+            driver.close()
+            driver.quit()
+        # except:
+        #     pass
 
 
 if __name__ == '__main__':
